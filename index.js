21255b07c106c929a035557ebbe4b71b9646e416
abhyudaya29 <abhyudayadev123@gamil.com>
2025-01-09 23:14:22 +0530

    index file

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("testing ")
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import axios from "axios";
+// import morgan from "morgan";
+import logger from "./logger.js";
+import morgan from "morgan";
+import databaseConnection from "./database-config/databaseConfig.js"
+const app=express();
+const morganFormat = ":method :url :status :response-time ms";
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors("*"));
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
+databaseConnection()
+app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server listening on ${process.env.PORT}`);
+    logger.info(`server listening on ${process.env.PORT}`);
+
+})
+
