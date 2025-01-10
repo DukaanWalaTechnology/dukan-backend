bae1142fadf33d4ea528fd70455f9782293898be
abhyudaya29 <abhyudayadev123@gamil.com>
2025-01-10 12:51:56 +0530

    Login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import logger from '../logger.js';
 import bcrypt from 'bcrypt';
-
+import jwt from "jsonwebtoken"
 const prisma = new PrismaClient();
 
 export const createUser = async (req, res) => {
@@ -48,3 +48,69 @@ export const createUser = async (req, res) => {
         });
     }
 };
+
+
+export const signIn=async(req,res)=>{
+    const{email,password}=req.body;
+
+    try {
+        
+        if(!email||!password){
+            return res.status(404).json({
+                success:false,
+                message:"All fields are required",
+            })
+        }
+        const checkUserExists=await prisma.user.findUnique({
+            where:{
+                email:email
+            }
+        })
+        console.log(checkUserExists,"user exist?");
+        if(!checkUserExists){
+            return res.status(404).json({
+                success:false,
+                message:"User not found",
+            })
+        }
+        const verifyPassword=await bcrypt.compare(password,checkUserExists.password);
+        if(!verifyPassword){
+            return res.status(401).json({
+                success:false,
+                message:"Invalid Password",
+            })
+        }
+        const payload={
+            email:checkUserExists.email,
+            id:checkUserExists.id,
+            name:checkUserExists.name
+        }
+        // generate jwt token
+        const token=jwt.sign(payload,process.env.JSON_WEB_TOKEN,{
+            expiresIn:"10h"
+        });
+        checkUserExists.password=undefined;
+        const options={
+            expires:new Date(Date.now()+3*24*60*60*1000),
+            httpOnly:true,
+        }
+        res.cookie("token",token,options).status(200).json({
+            success:true,
+            token,
+            userData:checkUserExists,
+            message:"User LoggedIn successfully"
+
+        })
+
+    
+    } catch (error) {
+        console.log("error while Logining User",error);
+        logger.error("error while Logging user",error);
+        return res.status(500).json({
+            message:"Error while Logging User",
+            success:false
+
+        })
+        
+    }
+}
