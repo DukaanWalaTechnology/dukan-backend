4e79c034b06da0b6c7d5e63c259495cf159f1e70
abhyudaya29 <abhyudayadev123@gamil.com>
2025-01-10 12:52:02 +0530

    login route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createUser } from '../controllers/auth.controller.js';
+import { createUser,signIn } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/sign-up', createUser);
+router.post('/sign-in', signIn);
 
 export default router;
