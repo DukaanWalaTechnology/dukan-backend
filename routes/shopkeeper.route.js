4ba632952ce2dd3c593410efa2d53f988ac10452
abhyudaya29 <abhyudayadev123@gamil.com>
2025-01-18 23:30:59 +0530

    shopkeeper routes

--- routes/shopkeeper.route.js
+++ routes/shopkeeper.route.js
@@ -3,6 +3,6 @@ import express from 'express';
 import { registerShop } from '../controllers/shop-controllers/register.controller.js';
 import { auth } from '../middlewares/auth.middleware.js';
 const router = express.Router();
-router.post('/shop/register', registerShop)
+router.post('/shop/register', registerShop);
 
 export default router;
