6970a579f0ca382b077d4336857b243f4ead77a9
abhyudaya29 <abhyudayadev123@gamil.com>
2025-01-18 23:31:08 +0530

    admin routes

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,8 +3,10 @@ import { createUser,signIn } from '../controllers/auth.controller.js';
 import { registerShop } from '../controllers/shop-controllers/register.controller.js';
 import { approveRequest } from '../controllers/admin-controllers/approveRequest.controller.js';
 import { fetchPendingrequest } from '../controllers/admin-controllers/fetchPendingrequest.controller.js';
+import { fetchSelectedShop } from '../controllers/admin-controllers/fetchSelectedShop.controller.js';
 const router = express.Router();
 
 router.patch('/shop-request/:requestId/approve', approveRequest)
 router.get('/shop-request',fetchPendingrequest)
+router.get('/shop-data/:shopId',fetchSelectedShop)
 export default router;
