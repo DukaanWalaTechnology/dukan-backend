ef43f4490d5a2b934e65b85e0a0044e684cd60cc
abhyudaya29 <abhyudayadev123@gamil.com>
2025-02-11 12:13:39 +0530

    fixed pending and approved shops

--- controllers/admin-controllers/fetchPendingrequest.controller.js
+++ controllers/admin-controllers/fetchPendingrequest.controller.js
@@ -5,7 +5,13 @@ import logger from "../../logger.js"
 const prisma = new PrismaClient();
 export const fetchPendingrequest=async(req,res)=>{
     try {
-        const pendingRquest=await prisma.shopRequest.findMany();
+        const pendingRquest=await prisma.shopRequest.findMany(
+            {
+                where:{
+                    status:"PENDING"
+                }
+            }
+        );
         console.log("Pending Requests",pendingRquest);
         if(!pendingRquest){
             return res.status(404).json({
@@ -29,4 +35,32 @@ export const fetchPendingrequest=async(req,res)=>{
         
     }
 
+}
+
+export const fetchRegisteredShops=async(req,res)=>{
+    try {
+        const shop=await prisma.shop.findMany();
+        console.log("registered Requests",shop);
+        if(!shop){
+            return res.status(404).json({
+                success: false,
+                message: 'No Registered Shops Found',
+            });
+        }
+        return res.json({
+            success:true,
+            message: 'Registered shops fetched successfully',
+            data:shop
+        })
+        
+    } catch (error) {
+        console.log("Error while fetching all shops",error);
+        logger.error("Error while fetching all shops",error);
+        return res.status(500).json({
+            message:"Error while fetching all shops",
+            success:false
+
+        })
+        
+    }
 }
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { createUser,logoutUser,signIn } from '../controllers/auth.controller.js';
 import { registerShop } from '../controllers/shop-controllers/register.controller.js';
 import { approveRequest } from '../controllers/admin-controllers/approveRequest.controller.js';
-import { fetchPendingrequest } from '../controllers/admin-controllers/fetchPendingrequest.controller.js';
+import { fetchPendingrequest, fetchRegisteredShops } from '../controllers/admin-controllers/fetchPendingrequest.controller.js';
 import { fetchSelectedShop } from '../controllers/admin-controllers/fetchSelectedShop.controller.js';
 const router = express.Router();
 
 router.patch('/shop-request/:requestId/approve', approveRequest)
 router.get('/shop-request',fetchPendingrequest)
 router.get('/shop-data/:shopId',fetchSelectedShop)
+router.get('/registered',fetchRegisteredShops)
 router.post('/logout', logoutUser);
 export default router;
