1a24a28773e32cdea3824507f9bc99589a30dbb1
abhyudaya29 <abhyudayadev123@gamil.com>
2025-02-12 05:27:06 +0530

    product fetcing api for user side

---
+++ controllers/user-contollers/fetchproducts.controller.js
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+import logger from "../../logger.js"
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
+const prisma = new PrismaClient();
+
+// need to be updated in future versions
+export const fetchProductsForUser=async(req,res)=>{
+    try {
+    const { userId } = req.params;
+    const fetchProducts = await prisma.product.findMany({
+        include:{shop:{
+            select:{
+                shopName:true,
+                address:true,
+                phone:true,
+                profileImage:true
+            }
+        }}
+    });
+    console.log("All products fetched:", fetchProducts);
+    if(!fetchProducts){
+        return res.status(404).json({
+            success: false,
+            message: 'No products found',
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: 'Products fetched successfully',
+        results: fetchProducts,
+    })
+
+        
+    } catch (error) {
+        console.error("Error while fetching products for user", error);
+        logger.error("Error while fetching products for user", error);
+        return res.status(500).json({
+            success: false,
+            message: "Error while fetching products for user",
+            error: error.message,
+        });
+        
+    }
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createUser,signIn } from '../controllers/auth.controller.js';
+import { fetchProductsForUser } from '../controllers/user-contollers/fetchproducts.controller.js';
 
 const router = express.Router();
 
 router.post('/sign-up', createUser);
 router.post('/sign-in', signIn);
-
+router.get('/fetchProducts', fetchProductsForUser);
 export default router;
